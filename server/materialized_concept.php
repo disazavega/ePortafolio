d5a68189fb6691a7ed47c99b7dc06dc1dd17c501
tdubourg <plop@plop.fake>
2013-12-15 15:27:37 +0100

    Fixed the message for delete action + indentation harmonized/fixed + code cleanup

--- server/materialized_concept.php
+++ server/materialized_concept.php
@@ -3,14 +3,6 @@
 include('smarty/Smarty.class.php');
 include('services.php');
 
-//      Test values
-/*      $_POST['action'] = 'delete';
-        $_POST['cm_id'] = '4';
-        $_POST['concept'] = '2';
-        $_POST['cm_name'] = 'MC Test3';
- */
-        
-
 function sanitize($string)
 {
 	return str_replace("'", "\'", $string);
@@ -19,7 +11,7 @@ function sanitize($string)
 // GUI for the materialization (conceptMaterialized creation)
 if ($_POST['action'] === 'new') {
 
-        // Call services
+	// Call services
 	$service = new Services();
 	$concepts = $service->ListConcepts();
 	
@@ -28,35 +20,34 @@ if ($_POST['action'] === 'new') {
 
 	$smarty->assign('BASE_URL', 'http://127.0.0.1:8080');
 	$smarty->assign('cm', array(
-	    'name' => sanitize($current_concept_name)
+		'name' => sanitize($current_concept_name)
 	));
 	$list = array();
 
 	// Assign values
 	foreach ($concepts as $concept) {
-	 	$list[] = array(
-	 			'id' => $concept->id,
-	 			'name' => $concept->name
-	 		);
-	 } 
+		$list[] = array(
+				'id' => $concept->id,
+				'name' => $concept->name
+			);
+	}
 
-        // Assign concepts    
+	// Assign concepts    
 	$smarty->assign('concepts_list', $list);
 	// display it
 	$smarty->display('tpl/concept-mat-new.tpl');
 
 } 
-
 // GUI for the conceptMaterialized update
 else if ($_POST['action'] === 'edit' && is_numeric($_POST['cm_id'])) {
 
-    // Call services
-    $service = new Services();
+	// Call services
+	$service = new Services();
 	$concepts = $service->ListConcepts();
-    	
-    $matConcept = $service->RecoverMaterializedConcept($_POST['cm_id']);
-        
-    // Create object
+		
+	$matConcept = $service->RecoverMaterializedConcept($_POST['cm_id']);
+		
+	// Create object
 	$smarty = new Smarty;
 
 	$id = intval($_POST['cm_id']);
@@ -64,18 +55,18 @@ else if ($_POST['action'] === 'edit' && is_numeric($_POST['cm_id'])) {
 	$smarty->assign('BASE_URL', 'http://127.0.0.1:8080');
 	$smarty->assign('cm', array(
 		'id' => $id,
-	    'name' => sanitize($matConcept->name)
+		'name' => sanitize($matConcept->name)
 	));
 
 	$list = array();
 
-    // Assign values
+	// Assign values
 	foreach ($concepts as $concept) {
-	 	$list[] = array(
- 			'id' => $concept->id,
- 			'name' => $concept->name,
-            'checked' => ($concept->id === $matConcept->idConcept) ? "checked='checked'" : ""
- 		);
+		$list[] = array(
+			'id' => $concept->id,
+			'name' => $concept->name,
+			'checked' => ($concept->id === $matConcept->idConcept) ? "checked='checked'" : ""
+		);
 	 } 
 
 	$smarty->assign('concepts_list', $list);
@@ -96,27 +87,27 @@ else if ($_POST['action'] === 'edit' && is_numeric($_POST['cm_id'])) {
 		echo 'Update MC OK';
 	}
 } else if ($_POST['action'] === 'create') { // "create" is the submit action of "new"
-        $service = new Services();
-    
-        $concept_id = intval($_POST['concept']);
+	$service = new Services();
+
+	$concept_id = intval($_POST['concept']);
 	$cm_name = $_POST['cm_name'];
-        
-        // TODO -  Add the id Form
-        $form_id = intval('1');
+		
+	// TODO -  Add the id Form
+	$form_id = 1;
 
 	$res = $service->MaterializeConcept($form_id, $concept_id, $cm_name);
-        if (!$res) {
+	if (!$res) {
 		echo 'Error create MC test message!';
 	} else {
 		echo 'Create MC OK';
 	}
 } else if ($_POST['action'] === 'delete') { 
 	$service = new Services();
-        $cm_id = intval($_POST['cm_id']);
+	$cm_id = intval($_POST['cm_id']);
 
 	$res = $service->UnMaterializeConcept($cm_id);
-        if (! $res) {
-                echo 'Delete MC OK';
+	if (!$res) {
+		echo 'OK';
 	} else {
 		echo 'Error delete MC test message!';
 	}
