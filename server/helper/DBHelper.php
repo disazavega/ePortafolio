bec4810ff05b196cd4a193531ed0a1a5404286a1
tdubourg <plop@plop.fake>
2013-12-15 12:00:21 +0100

    Added logging to see whats going on with the DB

--- server/helper/DBHelper.php
+++ server/helper/DBHelper.php
@@ -1,15 +1,19 @@
 <?php
 
+require_once dirname(__FILE__) . "/LogHelper.class.php";
+
 class DBHelper {
 
   private static $dbHelper;
   private $mysqli;
   private $db;
+  private $logger;
   
   private function __construct() {
     $config = require __DIR__.'/../config.php';
     $this->mysqli = new mysqli($config['dbhost'], $config['username'], $config['password']) or die("Could not connect");
     $this->db = $this->mysqli->select_db ($config['db']) or die("Database does not exist or no permission");
+    $this->logger = LogHelper::instance(dirname(__FILE__) . "/../logs", LogHelper::DEBUG);
   }
   
   public static function getInstance() {
@@ -26,7 +30,7 @@ class DBHelper {
   }
 
   public function selectQuery($query) {
-    $result =  $this->mysqli->query($query);
+    $result =  $this->execute_query($query);
     $response = array();
     if ($result) {
       while ($row = mysqli_fetch_assoc($result)) {
@@ -37,13 +41,23 @@ class DBHelper {
   }
   
   public function insertUpdateQuery($query) {
-    $result =  $this->mysqli->query($query);
+    $result = $this->execute_query($query);
     $id = $this->mysqli->insert_id;
     return $id;
   }
   
   public function deleteQuery($query) {
-    $this->mysqli->query($query);
+    $this->execute_query($query);
+  }
+
+  private function execute_query($query)
+  {
+    $this->log_query($query);
+    return $this->mysqli->query($query);
+  }
+
+  private function log_query($query) {
+    $this->logger->logDebug("Query Executed: " . $query);
   }
 }
 
