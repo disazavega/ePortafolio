c3759e40d6e268b3aeae9d4577b6cbb68f431830
tdubourg <plop@plop.fake>
2013-12-17 10:36:27 +0100

    Fixed some broken indentation

--- server/alignment.php
+++ server/alignment.php
@@ -13,15 +13,15 @@ if ($_POST['action'] === 'list' && is_numeric($_POST['cm_id'])) {
     $alignments = $service->ListAlignmentsMC($cm_id);
     
     // create object
-	$smarty = new Smarty;
+    $smarty = new Smarty;
         
-	$smarty->assign('BASE_URL', 'http://127.0.0.1:8080');
-	$smarty->assign('cm', array(
-		'id' => $mat_concept->id, 
+    $smarty->assign('BASE_URL', 'http://127.0.0.1:8080');
+    $smarty->assign('cm', array(
+        'id' => $mat_concept->id, 
         'name' => sanitize($mat_concept->name)
-	));
+    ));
         
-	$list = array();
+    $list = array();
         
     // Assign values
     foreach ($alignments as $alignment) {
@@ -32,8 +32,8 @@ if ($_POST['action'] === 'list' && is_numeric($_POST['cm_id'])) {
     }
 
     $smarty->assign('alignments_list', $list);
-	// display it
-	$smarty->display('tpl/alignment-list.tpl');
+    // display it
+    $smarty->display('tpl/alignment-list.tpl');
 } else if ($_POST['action'] === 'edit' && is_numeric($_POST['alignment_id'])) {
     
     // Call services
@@ -56,51 +56,52 @@ if ($_POST['action'] === 'list' && is_numeric($_POST['cm_id'])) {
     
     //First smarty assing
 
-	$smarty->assign('BASE_URL', 'http://127.0.0.1:8080');
+    $smarty->assign('BASE_URL', 'http://127.0.0.1:8080');
         // Normaly thats no the
-	$smarty->assign('cm', array(
-		'id' => $this_alignment->id,
-	    'name' => sanitize("No Name right ?")
-	));
+    $smarty->assign('cm', array(
+        'id' => $this_alignment->id,
+        'name' => sanitize("No Name right ?")
+    ));
 
-	$list_fields = array();
-	$list_attributes = array();
+    $list_fields = array();
+    $list_attributes = array();
         
-        //Assignation field
-        foreach ($fields as $field) {
-            $list_fields[] = array(
-		'id' => $field->id,
-		'name' => sanitize($field->name),
-                'checked' => ($field->id === $this_alignment->idField) ? "checked='checked'" : ""
-            );
-        }
-        
-        //Assignation attributes
-        foreach ($attributes as $attribute) {
-            $list_attributes[] = array(
-                'id' => $attribute->id,
-                'name' => sanitize($attribute->name),
-                'checked' => ($attribute->id === $this_alignment->idAttribute) ? "checked='checked'" : ""
-            );
-        }
-	$smarty->assign('attributes_list', $list_attributes);
-	$smarty->assign('fields_list', $list_fields);
+    //Assignation field
+    foreach ($fields as $field) {
+        $list_fields[] = array(
+            'id' => $field->id,
+            'name' => sanitize($field->name),
+            'checked' => ($field->id === $this_alignment->idField) ? "checked='checked'" : ""
+        );
+    }
+    
+    //Assignation attributes
+    foreach ($attributes as $attribute) {
+        $list_attributes[] = array(
+            'id' => $attribute->id,
+            'name' => sanitize($attribute->name),
+            'checked' => ($attribute->id === $this_alignment->idAttribute) ? "checked='checked'" : ""
+        );
+    }
+    
+    $smarty->assign('attributes_list', $list_attributes);
+    $smarty->assign('fields_list', $list_fields);
 
-	// display it
-	$smarty->display('tpl/alignment-edit.tpl');
+    // display it
+    $smarty->display('tpl/alignment-edit.tpl');
 
 } else if ($_POST['action'] === 'update' && is_numeric($_POST['alignment_id']) && is_numeric($_POST['attribute_id']) && is_numeric($_POST['field_id'])) {
-	$service = new Services();
-	$alignment_id = intval($_POST['alignment_id']);
-	$attr_id = intval($_POST['attribute_id']);
-	$field_id = intval($_POST['field_id']);
-	
-        $res = $service->UpdateAlignment($alignment_id, $field_id, $attr_id);
-        if (!$res) {
-		echo 'Error update alignment test message!';
-	} else {
-		echo 'OK';
-	}
+    $service = new Services();
+    $alignment_id = intval($_POST['alignment_id']);
+    $attr_id = intval($_POST['attribute_id']);
+    $field_id = intval($_POST['field_id']);
+    
+    $res = $service->UpdateAlignment($alignment_id, $field_id, $attr_id);
+    if (!$res) {
+        echo 'Error update alignment test message!';
+    } else {
+        echo 'OK';
+    }
 
 } else if ($_POST['action'] === 'new' && is_numeric($_POST['cm_id'])) { /* we need the CM id in order to create an alignment, so that we can display the right options... */
 
@@ -108,7 +109,7 @@ if ($_POST['action'] === 'list' && is_numeric($_POST['cm_id'])) {
     $service = new Services();
     $alignment_id = intval($_POST['alignment_id']);
     $cm_id = intval($_POST['cm_id']);
-// TO DO : Add the form id
+    // TO DO : Add the form id
     $form_id = intval("1");
         
     //Load
@@ -116,67 +117,67 @@ if ($_POST['action'] === 'list' && is_numeric($_POST['cm_id'])) {
     $attributes = $service->ListAttributesConceptMaterialized($cm_id);
     $fields = $service->ListFieldsForm($form_id);
 
-        // create object
-	$smarty = new Smarty;
+    // create object
+    $smarty = new Smarty;
 
-	$id = intval($_POST['cm_id']);
+    $id = intval($_POST['cm_id']);
 
-	$smarty->assign('BASE_URL', 'http://127.0.0.1:8080');
-	$smarty->assign('cm', array(
-		'id' => $id,
-	    'name' => sanitize("Normaly there s no name, right !")
-	));
+    $smarty->assign('BASE_URL', 'http://127.0.0.1:8080');
+    $smarty->assign('cm', array(
+        'id' => $id,
+        'name' => sanitize("Normaly there s no name, right !")
+    ));
 
-	$list_fields = array();
-	$list_attributes = array();
+    $list_fields = array();
+    $list_attributes = array();
         
-        //Assignation field
-        foreach ($fields as $field) {
-            $list_fields[] = array(
-		'id' => $field->id,
-		'name' => sanitize($field->name)
-            );
-        }
-        
-        //Assignation attributes
-        foreach ($attributes as $attribute) {
-            $list_attributes[] = array(
-                'id' => $attribute->id,
-                'name' => sanitize($attribute->name)
-            );
-        }
-	$smarty->assign('attributes_list', $list_attributes);
-	$smarty->assign('fields_list', $list_fields);
+    //Assignation field
+    foreach ($fields as $field) {
+        $list_fields[] = array(
+    'id' => $field->id,
+    'name' => sanitize($field->name)
+        );
+    }
+    
+    //Assignation attributes
+    foreach ($attributes as $attribute) {
+        $list_attributes[] = array(
+            'id' => $attribute->id,
+            'name' => sanitize($attribute->name)
+        );
+    }
+    $smarty->assign('attributes_list', $list_attributes);
+    $smarty->assign('fields_list', $list_fields);
 
-	// display it
-	$smarty->display('tpl/alignment-new.tpl');
+    // display it
+    $smarty->display('tpl/alignment-new.tpl');
 } else if ($_POST['action'] === 'create' 
-	&& is_numeric($_POST['cm_id']) /* we need the CM id in order to create an alignment, so that we can display the right options... */
-	&& is_numeric($_POST['attribute_id']) 
-	&& is_numeric($_POST['field_id'])
-	) {
+    && is_numeric($_POST['cm_id']) /* we need the CM id in order to create an alignment, so that we can display the right options... */
+    && is_numeric($_POST['attribute_id']) 
+    && is_numeric($_POST['field_id'])
+    ) {
     $service = new Services();
     
-	$cm_id = intval($_POST['cm_id']);
-	$attr_id = intval($_POST['attribute_id']);
-	$field_id = intval($_POST['field_id']);
-	
-        $res = $service->CreateAlignment($cm_id, $field_id, $attr_id);
-        if (!$res) {
-		echo 'Error create alignemnt test message!';
-	} else {
-		echo 'OK';
-	}
+    $cm_id = intval($_POST['cm_id']);
+    $attr_id = intval($_POST['attribute_id']);
+    $field_id = intval($_POST['field_id']);
+    
+    $res = $service->CreateAlignment($cm_id, $field_id, $attr_id);
+    if (!$res) {
+        echo 'Error create alignemnt test message!';
+    } else {
+        echo 'OK';
+    }
 } else if ($_POST['action'] === 'delete') { // "create" is the submit action of "new"
-	$service = new Services();
+    $service = new Services();
         $alignment_id = intval($_POST['alignment_id']);
         
-        $res = $service->DeleteAlignment($alignment_id);
-        if ($res) {
-		echo 'OK';
-	} else {
-		echo 'Error delete alignment test message! Error is' . $service->get_error($res);
-	}
+    $res = $service->DeleteAlignment($alignment_id);
+    if ($res) {
+        echo 'OK';
+    } else {
+        echo 'Error delete alignment test message! Error is' . $service->get_error($res);
+    }
 }
 
 ?>
